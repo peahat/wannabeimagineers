6ba8594150329a19a696bbf23e9dc6f8a5b1212c
Stephen Rooney <Stephen@Stephen.(none)>
2014-03-14 17:17:25 +0000

    Fixed styling in blog section.

--- wp-content/themes/wannabeimagineers/home.php
+++ wp-content/themes/wannabeimagineers/home.php
@@ -1,21 +1,27 @@
 <?php get_header(); ?>
 
-<p>This is the home.php</p>
+<!--<p>This is the home.php</p> -->
+<div class= "container">
+	<div class= 'blog-post'>
 
-<div class= 'blog-post'>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-	<h3><?php the_title(); ?></h3>
-	<?php the_content(); ?>
+	<div id="blogposttitle">
+		<h3><?php the_title(); ?></h3>
+	</div>
+	<div id="blogpostcontent">
+		<?php the_content(); ?>
+	</div>
 	
 
-<?php endwhile; else: ?>
 
-	<p>There are no posts or pages here</p>
+	<?php endwhile; else: ?>
 
-<?php endif; ?>
+		<p>There are no posts or pages here</p>
 
+	<?php endif; ?>
+
+	</div>
 </div>
 
 <?php get_footer(); ?>
