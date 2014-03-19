987e0404374099e447cd8672919b80338403a564
Stephen Rooney <Stephen@Stephen.(none)>
2014-03-19 18:37:00 +0000

    Backing up before styling

--- wp-content/themes/wannabeimagineers/home.php
+++ wp-content/themes/wannabeimagineers/home.php
@@ -9,8 +9,17 @@
 	<div id="blogposttitle">
 		<h3><?php the_title(); ?></h3>
 	</div>
+
+	
+
 	<div id="blogpostcontent">
+		
+		<small><?php the_time('F jS, Y') ?> by <?php the_author() ?></small>
+
 		<?php the_content(); ?>
+
+		<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?></p>
+
 	</div>
 	
 
